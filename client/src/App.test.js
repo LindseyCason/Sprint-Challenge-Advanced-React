f2050891fa5a8a5a602097c442ccb44558246833
Lindsey Cason <lindseyacason@gmail.com>
2019-09-06 12:47:23 -0500

    mvp

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,38 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Players from './players';
+import  FavePlayer  from "./form"
+import { render } from "@testing-library/react";
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
 });
 
-test("contains united states", () => {
-  //Arrange is const.container render line below
-  const app = render(<App />);
-  // console.log(container);
-  //Act is Getting the node by text getByText
-  app.getByText(/united states/i);
-
-  //Assert is getByText
+test("contains players", () => {
+  const container = render(<Players />);
+  container.getByText(/PlAyErS/i);
 });
 
-est("contains brazil", () => {
-  //Arrange is const.container render line below
+test("contains who do you choose", () => {
   const app = render(<App />);
-  // console.log(container);
-  //Act is Getting the node by text getByText
-  app.getByText(/brazil/i);
-
-  //Assert is getByText
-});
-
-est("contains searches", () => {
-  //Arrange is const.container render line below
-  const app = render(<App />);
-  // console.log(container);
-  //Act is Getting the node by text getByText
-  app.getByText(/searches/i);
-  //Assert is getByText
+  app.getByPlaceholderText(/Who do you choose/i);
 });
